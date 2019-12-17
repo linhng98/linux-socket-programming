e0e996a3d50da17e91f20afadea9986fe9d55648
nobabykill <linh1612340@gmail.com>
2019-12-17 16:47:23 +0700

    init load balancer project

---
+++ load-balancer/balancer.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, const char *argv[]) { return 0; }
