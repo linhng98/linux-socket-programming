6ab4ab1d72ead3bab8592b879a03b4277008043b
nobabykill <linh1612340@gmail.com>
2019-12-18 15:19:02 +0700

    add option commandline parameter

--- load-balancer/balancer.c
+++ load-balancer/balancer.c
@@ -1,8 +1,65 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <string.h>
 #include <arpa/inet.h>
+#include <getopt.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
-int main(int argc, const char *argv[]) { return 0; }
+#define BUFFSIZE 512
+#define USAGE "Usage: balancer [-p <PORT>]\n"
+#define VERSION                                                                                    \
+    "balancer v1.0.0\n"                                                                            \
+    "Written by Linh, Khue\n"
+#define HELP                                                                                       \
+    "balancer [-p <PORT>]\n"                                                                       \
+    "Load balancer forward request to webserver\n"                                                 \
+    "\n"                                                                                           \
+    "-p, --port         listening port of webserver server (default 8888)\n"                       \
+    "-h, --help         display help message then exit\n"                                          \
+    "-v, --version      output version information then exit\n"                                    \
+    "\n"
+
+typedef struct sockaddr_in sockaddr_in;
+typedef struct sockaddr sockaddr;
+
+static struct option long_options[] = {{"port", required_argument, 0, 'p'},
+                                       {"help", no_argument, 0, 'h'},
+                                       {"version", no_argument, 0, 'v'},
+                                       {0, 0, 0, 0}};
+
+static int port = 8888; // default load balancer port
+
+int main(int argc, char *argv[])
+{
+    int c;
+    int option_index;
+
+    while (1)
+    {
+        c = getopt_long(argc, argv, "p:hv", long_options, &option_index);
+        if (c == -1) // end of options
+            break;
+        switch (c)
+        {
+        case 'p':
+            port = (int)strtol(optarg, NULL, 10);
+            break;
+        case 'h':
+            printf(HELP);
+            exit(EXIT_SUCCESS);
+        case 'v':
+            printf(VERSION);
+            exit(EXIT_SUCCESS);
+        case '?':
+            printf("Try 'client --help' for more information.\n");
+            exit(EXIT_FAILURE);
+        default:
+            abort();
+        }
+    }
+
+    printf("port: %d\n", port);
+
+    exit(EXIT_SUCCESS);
+}
--- load-balancer/webserver.c
+++ load-balancer/webserver.c
@@ -1,8 +1,88 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <string.h>
 #include <arpa/inet.h>
+#include <getopt.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
-int main(int argc, const char *argv[]) { return 0; }
+#define BUFFSIZE 512
+#define USAGE "Usage: webserver [-p <PORT>] [-d <RESOURCE_DIR>]\n"
+#define VERSION                                                                                    \
+    "webserver v1.0.0\n"                                                                           \
+    "Written by Linh, Khue\n"
+#define HELP                                                                                       \
+    "webserver [-p <PORT>] [-d <RESOURCE_DIR>]\n"                                                  \
+    "Serve GET request from client\n"                                                              \
+    "\n"                                                                                           \
+    "-p, --port         listening port of webserver server (default 11111)\n"                      \
+    "-d, --dir          resource dir (default ./)\n"                                               \
+    "-h, --help         display help message then exit\n"                                          \
+    "-v, --version      output version information then exit\n"                                    \
+    "\n"
+
+typedef struct sockaddr_in sockaddr_in;
+typedef struct sockaddr sockaddr;
+
+static int is_dir(char *path);
+
+static struct option long_options[] = {{"port", required_argument, 0, 'p'},
+                                       {"dir", required_argument, 0, 'd'},
+                                       {"help", no_argument, 0, 'h'},
+                                       {"version", no_argument, 0, 'v'},
+                                       {0, 0, 0, 0}};
+
+static int port = 11111; // default webserver port
+static char *dir = ".";  // default resource dir
+
+int main(int argc, char *argv[])
+{
+    int c;
+    int option_index;
+
+    while (1)
+    {
+        c = getopt_long(argc, argv, "p:d:hv", long_options, &option_index);
+        if (c == -1) // end of options
+            break;
+        switch (c)
+        {
+        case 'p':
+            port = (int)strtol(optarg, NULL, 10);
+            break;
+        case 'd':
+            dir = optarg;
+            if (is_dir(dir) == 0) // invalid dir
+            {
+                printf("%s is invalid dir\n", dir);
+                exit(EXIT_FAILURE);
+            }
+            if (dir[strlen(dir) - 1] == '/') // strip off '/' character if exist
+                dir[strlen(dir) - 1] = '\0';
+            break;
+        case 'h':
+            printf(HELP);
+            exit(EXIT_SUCCESS);
+        case 'v':
+            printf(VERSION);
+            exit(EXIT_SUCCESS);
+        case '?':
+            printf("Try 'client --help' for more information.\n");
+            exit(EXIT_FAILURE);
+        default:
+            abort();
+        }
+    }
+
+    printf("dir: %s\nport: %d\n", dir, port);
+
+    exit(EXIT_SUCCESS);
+}
+
+int is_dir(char *path)
+{
+    struct stat path_stat;
+    stat(path, &path_stat);
+    return S_ISDIR(path_stat.st_mode);
+}
